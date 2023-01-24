50ccfb68983baaea9020f819b7cf4fe9e22bc839
Mitch Chanza <mitch.chanza@gmail.com>
2023-01-24 08:20:40 +0200

    feat: use main service in the entry file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-export {}
+import core from "@actions/core";
+import github from "@actions/github";
+
+import { Deplono } from  "./services";
+
+const type = core.getInput("type") as string;
+const credetials = core.getInput("credentials");
+/** Extract The main payload */
+const data = JSON.stringify(github.context.payload,null,2)
+Deplono.run(type, credetials, data);
